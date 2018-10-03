98bff2271d1e809145bbe25059e5bac0c27db378
Harvey West <harvey_west@live.com>
2018-10-03 07:47:15 +0100

    First commit that runs and delivers something to clients

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var open = require('open');
 
 var port = 3000;
 var app = express();
@@ -11,13 +10,14 @@ app.set('port', (process.env.port || port));
 app.disable('x-powered-by');
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../src/index.js'));
 });
 
 app.listen(process.env.PORT || app.get('port'), function (err) {
       if (err) {
         console.log(err);
       } else {
-        open('htt')
+        console.log("Node app is running at" +
+        app.get('host') + " : " + app.get('port'));
       }
     })
